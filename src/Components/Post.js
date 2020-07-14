1fea662c57b02a1c05c83b431644caa064f220c4
Emily Aquin <eaquin1@gmail.com>
2020-07-14 15:10:42 -0500

    added deleting comments

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,23 +1,40 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import PostForm from "./PostForm";
+import CommentForm from "./CommentForm";
 function Post({ blogs, editPost }) {
     const { postId } = useParams();
     /** Get the correct post */
     const thisBlog = blogs.filter((blog) => blog.id === postId);
     const [isEditing, setIsEditing] = useState(false);
-
+    const [comments, setComments] = useState("");
     /** Toggle editing on/off */
     const toggleEdit = () => {
         setIsEditing((edit) => !edit);
     };
 
     const edit = ({ title, description, body }) => {
-        editPost({ id: thisBlog[0].id, title: title, description: description, body: body });
+        editPost({
+            id: thisBlog[0].id,
+            title: title,
+            description: description,
+            body: body,
+        });
 
         toggleEdit();
     };
 
+    const addComment = (comment) => {
+        setComments((c) => [...c, comment]);
+    };
+
+    const handleDelete = (c) => {
+    
+        const commentIdx = comments.indexOf(c)
+        const commentCopy = [...comments]
+        commentCopy.splice(commentIdx, 1)
+        setComments(commentCopy)
+    }
     return (
         <div className="container">
             {isEditing ? (
@@ -32,8 +49,20 @@ function Post({ blogs, editPost }) {
                     <i className="fas fa-times text-danger" />
                     <p>{thisBlog[0].description}</p>
                     <p>{thisBlog[0].body}</p>
+                    {comments.length > 0
+                        ? comments.map((c) => (
+                              <div key={c}>
+                                  <p >{c}</p>
+                                  <i
+                                      className="fa fa-times text-danger mr-2"
+                                      onClick={() => handleDelete(c)}
+                                  />
+                              </div>
+                          ))
+                        : ""}
                 </>
             )}
+            <CommentForm addComment={addComment} />
         </div>
     );
 }
